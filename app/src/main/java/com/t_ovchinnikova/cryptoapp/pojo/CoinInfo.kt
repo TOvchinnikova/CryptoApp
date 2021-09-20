5c6a6152dd1fe472ea78e63599669e23b1335e09
TOvchinnikova <gibadullinatanya@gmail.com>
2021-09-20 21:12:05 +0500

    Local storage created (Room database)

--- app/src/main/java/com/t_ovchinnikova/cryptoapp/pojo/CoinInfo.kt
+++ app/src/main/java/com/t_ovchinnikova/cryptoapp/pojo/CoinInfo.kt
@@ -10,19 +10,7 @@ import android.media.Rating
 
 
 data class CoinInfo (
-    @SerializedName("Id")
-    @Expose
-    val id: String? = null,
-
     @SerializedName("Name")
     @Expose
-    val name: String? = null,
-
-    @SerializedName("FullName")
-    @Expose
-    val fullName: String? = null,
-
-    @SerializedName("ImageUrl")
-    @Expose
-    val imageUrl: String? = null
+    val name: String? = null
 )
--- app/src/main/java/com/t_ovchinnikova/cryptoapp/pojo/CoinPriceInfo.kt
+++ app/src/main/java/com/t_ovchinnikova/cryptoapp/pojo/CoinPriceInfo.kt
@@ -1,10 +1,12 @@
 package com.t_ovchinnikova.cryptoapp.pojo
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.Expose
 
 import com.google.gson.annotations.SerializedName
 
-
+@Entity(tableName = "full_price_list")
 data class CoinPriceInfo (
     @SerializedName("TYPE")
     @Expose
@@ -12,6 +14,7 @@ data class CoinPriceInfo (
     @SerializedName("MARKET")
     @Expose
     val market: String? = null,
+    @PrimaryKey
     @SerializedName("FROMSYMBOL")
     @Expose
     val fromSymbol: String? = null,
